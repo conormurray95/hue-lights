1da40cd666c33162db613e20540db919f35e1edf
Conor Murray <conor.murray@puppet.com>
2018-12-03 19:11:37 +0000

    Add app

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react'
-import { get, put } from '../Http/Http.js'
+import { put } from '../Http/Http.js'
 import './App.css'
 class App extends Component {
   constructor () {
     super()
+    // store any variables here
     this.state = {
       on: true
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.turnLightOff = this.turnLightOff.bind(this)
   }
-  handleClick () {
-    get('/lights')
-      .then(response => console.log(response))
-    }
+
   turnLightOff() {
-    put('/lights/1/state', {"on":!this.state.on}).
-    then(this.setState({on: !this.state.on}));
+    put('/lights/1/state', {"on":!this.state.on})
+    .then(this.setState({on: !this.state.on}));  // store the light state
   }
 
   render () {
     return (
       <div className='button__container'>
-        <button className='button' onClick={this.turnLightOff}>
-          Click Me
+        <button className='button' onClick={this.turnLightOff}> {/* when button is clicked call the turnLightOff() function */}
+          Click me
         </button>
         <p>{this.state.username}</p>
       </div>
